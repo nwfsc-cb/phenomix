5bdd9128d46e74ae374b86cb92bd120e5568e4d4
eric-ward <eric.ward@noaa.gov>
2020-03-10 11:38:14 -0700

    add log space argument to plotting

--- R/plot_diagnostics.R
+++ R/plot_diagnostics.R
@@ -4,9 +4,10 @@
 #'
 #' @param fitted A fitted model object
 #' @param type A plot type for ggplot, either "timing" or "scatter"
+#' @param logspace whether to plot the space in log space, defaults to TRUE
 #' @import ggplot2
 #' @export
-plot_diagnostics <- function(fitted, type="timing") {
+plot_diagnostics <- function(fitted, type="timing", logspace = TRUE) {
 
   # rebuild data frame
   df = data.frame(y=fitted$data_list$y,
@@ -17,24 +18,44 @@ plot_diagnostics <- function(fitted, type="timing") {
   # join in mean
   mus = data.frame(years = unique(df$years),
     mu = fitted$sdreport$value[which(names(fitted$sdreport$value)=="mu")])
-  df = dplyr::left_join(df,mus)
+  df = left_join(df,mus)
   df$timing = as.factor(ifelse(df$x<df$mu,"pre","post"))
 
   if(type=="scatter") {
-    g = ggplot2::ggplot(df, aes(pred,log(y),fill=timing,col=timing)) +
+    if(logspace==TRUE) {
+    g = ggplot(df, aes(pred,log(y),fill=timing,col=timing)) +
       geom_point(alpha=0.5) +
       facet_wrap(~years,scales="free") +
       geom_abline(intercept=0,slope=1) +
       xlab("Ln predicted") +
       ylab("Ln obs")
+    } else {
+        g = ggplot(df, aes(exp(pred),y,fill=timing,col=timing)) +
+          geom_point(alpha=0.5) +
+          facet_wrap(~years,scales="free") +
+          geom_abline(intercept=0,slope=1) +
+          xlab("Ln predicted") +
+          ylab("Ln obs")
+    }
+
   }
   if(type=="timing") {
-    g = ggplot2::ggplot(df, aes(x, pred,fill=timing,col=timing)) +
+    if(logspace==TRUE) {
+    g = ggplot(df, aes(x, pred,fill=timing,col=timing)) +
       geom_line(alpha=0.5) +
       facet_wrap(~years,scales="free") +
       xlab("Calendar day") +
       ylab("Ln pred and obs") +
       geom_point(aes(x, log(y),fill=timing,col=timing),size=2)
+    } else {
+      g = ggplot(df, aes(x, exp(pred),fill=timing,col=timing)) +
+        geom_line(alpha=0.5) +
+        facet_wrap(~years,scales="free") +
+        xlab("Calendar day") +
+        ylab("Ln pred and obs") +
+        geom_point(aes(x, y,fill=timing,col=timing),size=2)
+
+    }
   }
   return(g)
 }

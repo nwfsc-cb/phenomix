7d188d1aad32b261c19f47d9591e6d5f7db12ef8
eric-ward <eric.ward@noaa.gov>
2020-03-14 20:33:34 -0700

    updated tests

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -3,25 +3,42 @@ context("Fitting")
 set.seed(1)
 
 test_that("gaussian model - symmetric works", {
-  set.seed(1)
   fitted = fit(create_data(fishdist, asymmetric_model = FALSE))
     expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
 })
 
 test_that("gaussian model - asymmetric works", {
-  set.seed(1)
   fitted = fit(create_data(fishdist, asymmetric_model = TRUE))
   expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
 })
 
 test_that("t model - symmetric works", {
-  set.seed(1)
   fitted = fit(create_data(fishdist, asymmetric_model = FALSE, est_t_model = TRUE))
   expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
 })
 
 test_that("t model - asymmetric works", {
-  set.seed(1)
   fitted = fit(create_data(fishdist, asymmetric_model = TRUE, est_t_model = TRUE))
   expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
 })
+
+test_that("gaussian model - symmetric no slope sigma", {
+  fitted = fit(create_data(fishdist, asymmetric_model = FALSE, est_sigma_trend = FALSE))
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+})
+
+test_that("gaussian model - asymmetric no slope sigma", {
+  fitted = fit(create_data(fishdist, asymmetric_model = TRUE, est_sigma_trend = FALSE))
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+})
+
+test_that("gaussian model - symmetric no slope mu", {
+  fitted = fit(create_data(fishdist, asymmetric_model = FALSE, est_mu_trend = FALSE))
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+})
+
+test_that("gaussian model - asymmetric no slope mu", {
+  fitted = fit(create_data(fishdist, asymmetric_model = TRUE, est_mu_trend = FALSE))
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+})
+

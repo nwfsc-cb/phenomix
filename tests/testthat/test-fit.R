97ed0aa0364e60006e6f7ed0312e2eb6e8b6b3c1
Eric Ward <eric.ward@noaa.gov>
2021-04-06 14:46:33 -0700

    Update test-fit.R

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -1,48 +1,345 @@
 context("Fitting")
 
-test_that("gaussian model - symmetric works", {
+test_that("gaussian model - symmetric works - 1 year", {
   set.seed(1)
-  fitted <- fit(create_data(fishdist, asymmetric_model = FALSE), silent = TRUE)
-  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
-})
 
-test_that("gaussian model - asymmetric works", {
+  fitted <- fit(create_data(fishdist[which(fishdist$year == 1980), ],
+                            asymmetric_model = FALSE
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
   set.seed(1)
-  fitted <- fit(create_data(fishdist, asymmetric_model = TRUE), silent = TRUE)
+  fitted <- fit(create_data(fishdist[which(fishdist$year == 1980), ],
+                            asymmetric_model = FALSE, family = "poisson"
+  ),
+  silent = TRUE
+  )
   expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
-})
 
-test_that("t model - symmetric works", {
   set.seed(1)
-  fitted <- fit(create_data(fishdist, asymmetric_model = FALSE, tail_model = "student_t"), silent = TRUE)
+  fitted <- fit(create_data(fishdist[which(fishdist$year == 1980), ],
+                            asymmetric_model = FALSE,
+                            family = "negbin"
+  ),
+  silent = TRUE
+  )
   expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
 })
 
-test_that("t model - asymmetric works", {
-  set.seed(2)
-  fitted <- fit(create_data(fishdist, asymmetric_model = TRUE, tail_model = "student_t"), silent = TRUE)
-  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
-})
-
-test_that("gaussian model - symmetric no slope sigma", {
-  fitted <- fit(create_data(fishdist, asymmetric_model = FALSE, est_sigma_trend = FALSE), silent = TRUE)
-  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
-})
-
-test_that("gaussian model - asymmetric no slope sigma", {
+test_that("student-t model - symmetric works - 1 year", {
   set.seed(1)
-  fitted <- fit(create_data(fishdist, asymmetric_model = TRUE, est_sigma_trend = FALSE), silent = TRUE)
+  fitted <- fit(create_data(fishdist[which(fishdist$year == 1980), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            tail_model = "student_t"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year == 1980), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            tail_model = "student_t",
+                            family = "poisson"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year == 1980), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            tail_model = "student_t",
+                            family = "negbin"
+  ),
+  silent = TRUE
+  )
   expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
 })
 
-test_that("gaussian model - symmetric no slope mu", {
+test_that("gnorm model - symmetric works - 1 year", {
   set.seed(1)
-  fitted <- fit(create_data(fishdist, asymmetric_model = FALSE, est_mu_trend = FALSE), silent = TRUE)
+  fitted <- fit(create_data(fishdist[which(fishdist$year == 1980), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            tail_model = "gnorm"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year == 1980), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            tail_model = "gnorm",
+                            family = "poisson"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year == 1980), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            tail_model = "gnorm",
+                            family = "negbin"
+  ),
+  silent = TRUE
+  )
   expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
 })
 
-test_that("gaussian model - asymmetric no slope mu", {
+test_that("gaussian model - symmetric works - 10 years - mu trend", {
   set.seed(1)
-  fitted <- fit(create_data(fishdist, asymmetric_model = TRUE, est_mu_trend = FALSE), silent = TRUE)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1970), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = TRUE,
+                            est_sigma_re = FALSE,
+                            est_mu_re = TRUE
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1970), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = TRUE,
+                            est_sigma_re = FALSE,
+                            est_mu_re = TRUE, family =
+                              "poisson"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1970), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = TRUE,
+                            est_sigma_re = FALSE,
+                            est_mu_re = TRUE,
+                            family = "negbin"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+})
+
+test_that("gaussian model - symmetric works - 10 years - no trend but with re", {
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1970), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = FALSE,
+                            est_mu_re = TRUE
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1970), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = FALSE,
+                            est_mu_re = TRUE, family =
+                              "poisson"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1970), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = FALSE,
+                            est_mu_re = TRUE,
+                            family = "negbin"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+})
+
+test_that("gaussian model - symmetric works - 3 years - no trend but with re", {
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1977), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1977), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE,
+                            family = "poisson"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1977), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE,
+                            family = "negbin"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+})
+
+test_that("gaussian model - symmetric works - 3 years - trend with re", {
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1977), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = TRUE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1977), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = TRUE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE,
+                            family = "poisson"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1977), ],
+                            asymmetric_model = FALSE,
+                            est_sigma_trend = TRUE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE,
+                            family = "negbin"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+})
+
+
+test_that("gaussian model - asymmetric works - 3 years - no trend but with re", {
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1977), ],
+                            asymmetric_model = TRUE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1977), ],
+                            asymmetric_model = TRUE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE,
+                            family = "poisson"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1977), ],
+                            asymmetric_model = TRUE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE,
+                            family = "negbin"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+})
+
+
+test_that("gaussian model - asymmetric works - 10 years - trend with re", {
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1970), ],
+                            asymmetric_model = TRUE,
+                            est_sigma_trend = TRUE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1970), ],
+                            asymmetric_model = TRUE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE,
+                            family = "poisson"
+  ),
+  silent = TRUE
+  )
+  expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
+
+  set.seed(1)
+  fitted <- fit(create_data(fishdist[which(fishdist$year > 1970), ],
+                            asymmetric_model = TRUE,
+                            est_sigma_trend = FALSE,
+                            est_mu_trend = FALSE,
+                            est_sigma_re = TRUE,
+                            est_mu_re = FALSE,
+                            family = "negbin"
+  ),
+  silent = TRUE
+  )
   expect_equal(length(which(is.na(fitted$sdreport$sd))), 0)
 })

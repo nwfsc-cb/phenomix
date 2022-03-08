286498d46aef7ab2c73dafdbc40257c4abe98e64
ericward-noaa <eric.ward@noaa.gov>
2022-03-08 14:25:09 -0800

    sam's plot suggestions

--- R/plot_diagnostics.R
+++ R/plot_diagnostics.R
@@ -28,36 +28,76 @@ plot_diagnostics <- function(fitted, type = "timing", logspace = TRUE) {
 
   if (type == "scatter") {
     if (logspace == TRUE) {
+      if(fitted$data_list$family %in% c(2,3,5)) {
       g <- ggplot(df, aes(pred, log(y), fill = timing, col = timing)) +
         geom_point(alpha = 0.5) +
         facet_wrap(~years, scales = "free") +
         geom_abline(intercept = 0, slope = 1) +
         xlab("Ln predicted") +
         ylab("Ln obs")
+      }
+      if(fitted$data_list$family %in% c(1)) {
+        g <- ggplot(df, aes(log(pred), log(y), fill = timing, col = timing)) +
+          geom_point(alpha = 0.5) +
+          facet_wrap(~years, scales = "free") +
+          geom_abline(intercept = 0, slope = 1) +
+          xlab("Ln predicted") +
+          ylab("Ln obs")
+      }
     } else {
+      if(fitted$data_list$family %in% c(2,3,5)) {
       g <- ggplot(df, aes(exp(pred), y, fill = timing, col = timing)) +
         geom_point(alpha = 0.5) +
         facet_wrap(~years, scales = "free") +
         geom_abline(intercept = 0, slope = 1) +
         xlab("Ln predicted") +
         ylab("Ln obs")
+      }
+      if(fitted$data_list$family %in% c(1)) {
+        g <- ggplot(df, aes(pred, y, fill = timing, col = timing)) +
+          geom_point(alpha = 0.5) +
+          facet_wrap(~years, scales = "free") +
+          geom_abline(intercept = 0, slope = 1) +
+          xlab("Predicted") +
+          ylab("Obs")
+      }
     }
   }
   if (type == "timing") {
     if (logspace == TRUE) {
+      if(fitted$data_list$family %in% c(2,3,5)) {
       g <- ggplot(df, aes(x, pred, fill = timing, col = timing)) +
         facet_wrap(~years, scales = "free") +
         xlab("Calendar day") +
         ylab("Ln pred and obs") +
         geom_point(aes(x, log(y), fill = timing, col = timing), size = 1, alpha = 0.5) +
         geom_line(col = "black")
+      }
+      if(fitted$data_list$family %in% c(1)) {
+        g <- ggplot(df, aes(x, log(pred), fill = timing, col = timing)) +
+          facet_wrap(~years, scales = "free") +
+          xlab("Calendar day") +
+          ylab("Ln pred and obs") +
+          geom_point(aes(x, log(y), fill = timing, col = timing), size = 1, alpha = 0.5) +
+          geom_line(col = "black")
+      }
     } else {
+      if(fitted$data_list$family %in% c(2,3,5)) {
       g <- ggplot(df, aes(x, exp(pred), fill = timing, col = timing)) +
         facet_wrap(~years, scales = "free") +
         xlab("Calendar day") +
         ylab("Ln pred and obs") +
         geom_point(aes(x, y, fill = timing, col = timing), size = 1, alpha = 0.5) +
         geom_line(col = "black")
+      }
+      if(fitted$data_list$family %in% c(1)) {
+        g <- ggplot(df, aes(x, pred, fill = timing, col = timing)) +
+          facet_wrap(~years, scales = "free") +
+          xlab("Calendar day") +
+          ylab("Ln pred and obs") +
+          geom_point(aes(x, y, fill = timing, col = timing), size = 1, alpha = 0.5) +
+          geom_line(col = "black")
+      }
     }
   }
   return(g)

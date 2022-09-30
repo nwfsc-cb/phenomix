14e47a5204d331f4cc7c5ae03f569c386dd11269
ericward-noaa <eric.ward@noaa.gov>
2022-09-30 15:12:47 -0700

    Add Skagit chum data per Sam/Joe

--- R/data.R
+++ R/data.R
@@ -1,4 +1,12 @@
-#' Example data for fish distributions from multiple years
+#' Example simulate data for fish distributions from multiple years
+#'
+#' @format A data frame containing simulated data.
+"fishdist"
+
+#' Count data collected by Washington Department of Fish and Wildlife on
+#' chum salmon from the Skagit River (Washington state). Each row of the
+#' dataframe contains an observation ("number") on a given date ("date").
+#' The year ("year") and calendar day ("doy") are also included.
 #'
 #' @format A data frame.
-"fishdist"
+"chum"

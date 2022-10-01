467c7ebca318cf9d5a66b2184759306b0e7a1d15
Eric Ward <eric.ward@noaa.gov>
2022-10-01 09:26:04 -0700

    update for Sam

--- R/methods.R
+++ R/methods.R
@@ -41,19 +41,39 @@ extractAIC.phenomix <- function(fit, scale, k = 2, ...) {
   return(data.frame("df" = edf, "AIC" = -2 * L + k * edf))
 }
 
-#' Get fitted values from model object, copying glmmTMB 'fast' implementation
+#' Get predicted values from model object, copying glmmTMB 'fast' implementation
 #'
 #' @param fit The fitted phenomix model
+#' @param se.fit Boolean, whether to produce SEs defaults to FALSE
 #' @param ... Extra parameters
 #'
 #' @export
-fitted.phenomix <- function (object, ...) {
+predict.phenomix <- function (object, se.fit=FALSE, ...) {
   ee <- environment(object$obj$fn)
-  lp <- ee$last.par.best #
+  lp <- ee$last.par.best # best maximum likelihood estimate, similar to what glmmTMB uses
+
+  new_tmb_obj <- TMB::MakeADFun(
+    data = object$data_list,
+    parameters = ee$parList(lp),
+    map = ee$map,
+    random = ee$random,
+    DLL = "phenomix",
+    silent = TRUE
+  )
+  new_sdreport <- TMB::sdreport(new_tmb_obj)
+
+  indx <- which(names(new_sdreport$value)=="pred")
+  pred <- new_sdreport$value[indx]
+
   dat <- data.frame(y = ee$data$y,
                     yint = ee$data$yint,
                     x = ee$data$x,
                     years = ee$data$years,
-                    pred = object$obj$report(lp)$pred)
+                    pred = pred)
+
+  if(se.fit==TRUE) {
+    dat$`se.fit` <- new_sdreport$sd[indx]
+  }
+
   return(dat)
 }
